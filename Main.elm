8408262295d223e1f6b689fa061ee036b77e25b2
James Carey <james.anthony.carey@gmail.com>
2016-09-08 21:29:21 +0100

    feat: add model.Mode, Home or Edit with JSON encode/decode

--- Main.elm
+++ Main.elm
@@ -6,7 +6,8 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Html.Keyed as Keyed
 import Html.Lazy exposing (lazy, lazy2)
-import Json.Decode as Json
+import Json.Decode exposing ((:=), andThen)
+import Json.Encode
 import String
 
 main : Program Never
@@ -18,21 +19,49 @@ main =
     , subscriptions = subscriptions
     }
 
-port setStorage : Model -> Cmd msg
+port setStorage : Json.Encode.Value -> Cmd msg
 
 port focus : String -> Cmd msg
 
 subscriptions : Model -> Sub Msg
 subscriptions _ = Sub.none
 
+-- JSON
+-- ENCODE
+modelToValue : Model -> Json.Encode.Value
+modelToValue model =
+    Json.Encode.object
+        [ ("mode", modeToValue model.mode)
+        ]
+        
+modeToValue : Mode -> Json.Encode.Value
+modeToValue mode = Json.Encode.string (toString mode)
+
+-- DECODE
+modelDecoder : Json.Decode.Decoder Model
+modelDecoder =
+    Json.Decode.object1 Model
+        ("mode" := Json.Decode.string `andThen` modeDecoder)
+            
+modeDecoder : String -> Json.Decode.Decoder Mode
+modeDecoder mode =
+    case mode of
+        "Home" -> Json.Decode.succeed Home
+        "Edit" -> Json.Decode.succeed Edit
+        _      -> Json.Decode.fail (mode ++ " is not a recognized value for Mode")
+
 -- MODEL
+type Mode
+    = Home
+    | Edit
+      
 type alias Model =
-    { userName : String
+    { mode : Mode
     }
 
 emptyModel : Model
 emptyModel =
-  { userName = ""
+  { mode = Home
   }
 
 init : ( Model, Cmd Msg )
@@ -41,12 +70,18 @@ init = ( emptyModel, Cmd.none )
 -- UPDATE
 type Msg
     = NoOp
+    | StartEdit
+    | StopEdit
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
   case msg of
     NoOp ->
       model ! []
+    StartEdit ->
+      { model | mode = Edit } ! []
+    StopEdit ->
+      { model | mode = Home } ! []
 
 -- VIEW
 view : Model -> Html Msg
@@ -56,4 +91,7 @@ view model =
     , style []
     ]
     [ text "thelma"
+    , text ":"
+    , text (toString model.mode)      
     ]
+
